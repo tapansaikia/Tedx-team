004c3641cd1b1f8867f6d2d914bfb0b09df4d940
Tapan Saikia <37594259+tapansaikia@users.noreply.github.com>
2018-12-07 22:12:11 +0530

    Add files via upload

---
+++ tedx.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("myHeader").className = "pindown";
+    } else {
+        document.getElementById("myHeader").className = "";
+    }
+}
